1d885ed921fbce551035d4e145a907bbe084ddc7
Sahani Perera <sahanimadushi@gmail.com>
2023-08-11 00:32:52 +0530

    Update requestCountHelper.js

--- helpers/requestCountHelper.js
+++ helpers/requestCountHelper.js
@@ -1,1 +1,10 @@
+import * as CONFIG from '../config.js'
 
+export const getExpectedRequestCount = (throughput, timeunit) => {
+    return ((throughput / timeunit) * CONFIG.HOLD_DURATION) * CONFIG.THROUGHPUT_TOLERANCE_PCT;
+}
+
+export const isWithinHoldDuration = (testStartTime) => {
+    let currentDuration = Date.now() - testStartTime;
+    return currentDuration >= (CONFIG.RAMP_UP * 1000) && currentDuration < ((CONFIG.RAMP_UP + CONFIG.HOLD_DURATION) * 1000);
+}
